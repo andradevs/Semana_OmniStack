76e933d05c51b43e9526fdaafe91cd2591ff1794
andradevs <guilhermeandradevs@gmail.com>
2020-01-15 01:37:53 -0300

    Implementando a Base da Aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://artorias:fluminense@cluster0-2kcqr.mongodb.net/week10?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
+
+
+// Metodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de paremetros:
+// Query Params: req.query(Filtros, ordenação, paginação, ...)
+// Route Params: req.params(Identificar um recurso na alteração ou remoção)
+// Body: req.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (Não-relacional)
+
